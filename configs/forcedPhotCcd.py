c463bac56b08e519e481c68b4e5b92792a134736
Paul Price <price@astro.princeton.edu>
2020-02-10 14:49:35 -0500

    makeWarp/forcedPhotCcd: disable external calibrations
    Gen3 can't handle these yet, due to a bug in the butler.

--- configs/forcedPhotCcd.py
+++ configs/forcedPhotCcd.py
@@ -8,3 +8,8 @@ if os.path.exists(subaruConfig):
 hscConfig = os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "forcedPhotCcd.py")
 if os.path.exists(hscConfig):
     config.load(hscConfig)
+
+# Disable external calibrations: Gen3 isn't ready for these yet
+config.recalibrate.doApplyExternalPhotoCalib = False
+config.recalibrate.doApplyExternalSkyWcs = False
+config.recalibrate.doApplySkyCorr = False
--- configs/makeWarp.py
+++ configs/makeWarp.py
@@ -2,5 +2,4 @@ config.matchingKernelSize = 29
 config.makePsfMatched = True
 config.warpAndPsfMatch.psfMatch.kernel['AL'].alardSigGauss = [1.0, 2.0, 4.5]
 config.modelPsf.defaultFwhm = 7.7
-config.doApplySkyCorr = False
 config.doWriteEmptyWarps = True
