46d73bd8e9b491639c4d95237d8b8c098a027160
Hsin-Fang Chiang <hchiang@lsst.org>
2020-05-20 20:30:03 -0500

    Break pipetask-run into two steps
    This then also tests saving and using QuantumGraph.

--- bin/pipeline.sh
+++ bin/pipeline.sh
@@ -3,7 +3,10 @@ COLLECTION=shared/ci_hsc_output
 
 export DYLD_LIBRARY_PATH=$LSST_LIBRARY_PATH
 
-pipetask run -d "patch = 69" -j "$1" -b "$2"/butler.yaml \
+pipetask qgraph -q ci_hsc.pickle -d "patch = 69" -b "$2"/butler.yaml \
 -i calib/hsc,raw/hsc,masks/hsc,ref_cats,skymaps,shared/ci_hsc -o "$COLLECTION" \
---register-dataset-types \
 -p "$CI_HSC_GEN3_DIR"/pipelines/CiHsc.yaml
+
+pipetask run --qgraph ci_hsc.pickle -j "$1" -b "$2"/butler.yaml \
+-i calib/hsc,raw/hsc,masks/hsc,ref_cats,skymaps,shared/ci_hsc -o "$COLLECTION" \
+--register-dataset-types
