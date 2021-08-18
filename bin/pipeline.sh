00fc230a11e29b143d369a908104913a10aa6531
taranu <dtaranu@astro.princeton.edu>
2021-08-17 19:02:49 -0500

    Run faro after DRP

--- bin/pipeline.sh
+++ bin/pipeline.sh
@@ -5,6 +5,7 @@ set -e
 COLLECTION=HSC/runs/ci_hsc
 INPUTCOLL=HSC/defaults
 FAKES_COLLECTION=HSC/runs/ci_hsc_fakes
+FARO_COLLECTION=HSC/runs/ci_hsc_faro
 
 export DYLD_LIBRARY_PATH=$LSST_LIBRARY_PATH
 # exercise saving of the generated quantum graph to a file and reading it back
@@ -13,6 +14,9 @@ trap 'rm -f $QGRAPH_FILE' EXIT
 
 FAKES_QGRAPH_FILE=$(mktemp)_fakes.qgraph
 trap 'rm -f $FAKES_QGRAPH_FILE' EXIT
+
+FARO_QGRAPH_FILE=$(mktemp)_faro.qgraph
+trap 'rm -f $FARO_QGRAPH_FILE' EXIT
 
 pipetask qgraph \
     -d "skymap='discrete/ci_hsc' AND tract=0 AND patch=69" \
@@ -37,3 +41,16 @@ pipetask run -j "$1" -b "$2"/butler.yaml \
     --input "$COLLECTION" --output "$FAKES_COLLECTION" \
     --register-dataset-types \
     --qgraph "$FAKES_QGRAPH_FILE"
+
+pipetask qgraph \
+    -d "skymap='discrete/ci_hsc' AND tract=0 AND patch=69" \
+    -b "$2"/butler.yaml \
+    --input "$COLLECTION" --output "$FARO_COLLECTION" \
+    -p "$FARO_DIR"/pipelines/metrics_pipeline.yaml \
+    --save-qgraph "$FARO_QGRAPH_FILE"
+
+pipetask run -j "$1" -b "$2"/butler.yaml \
+    --input "$COLLECTION" --output "$FARO_COLLECTION" \
+    --register-dataset-types \
+    --qgraph "$FARO_QGRAPH_FILE"
+
