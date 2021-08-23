92bc34eadb1b07b274a1208d5b70aa7685ff8d70
taranu <dtaranu@astro.princeton.edu>
2021-08-23 12:00:48 -0500

    Add --long-log to pipetask calls

--- bin/pipeline.sh
+++ bin/pipeline.sh
@@ -18,38 +18,38 @@ trap 'rm -f $FAKES_QGRAPH_FILE' EXIT
 FARO_QGRAPH_FILE=$(mktemp)_faro.qgraph
 trap 'rm -f $FARO_QGRAPH_FILE' EXIT
 
-pipetask qgraph \
+pipetask --long-log qgraph \
     -d "skymap='discrete/ci_hsc' AND tract=0 AND patch=69" \
     -b "$2"/butler.yaml \
     --input "$INPUTCOLL" --output "$COLLECTION" \
     -p "$CI_HSC_GEN3_DIR"/pipelines/DRP.yaml \
     --save-qgraph "$QGRAPH_FILE"
 
-pipetask run -j "$1" -b "$2"/butler.yaml \
+pipetask --long-log run -j "$1" -b "$2"/butler.yaml \
     --input "$INPUTCOLL" --output "$COLLECTION" \
     --register-dataset-types \
     --qgraph "$QGRAPH_FILE"
 
-pipetask qgraph \
+pipetask --long-log qgraph \
     -d "skymap='discrete/ci_hsc' AND tract=0 AND patch=69" \
     -b "$2"/butler.yaml \
     --input "$COLLECTION" --output "$FAKES_COLLECTION" \
     -p "$CI_HSC_GEN3_DIR"/pipelines/DRPFakes.yaml \
     --save-qgraph "$FAKES_QGRAPH_FILE"
 
-pipetask run -j "$1" -b "$2"/butler.yaml \
+pipetask --long-log run -j "$1" -b "$2"/butler.yaml \
     --input "$COLLECTION" --output "$FAKES_COLLECTION" \
     --register-dataset-types \
     --qgraph "$FAKES_QGRAPH_FILE"
 
-pipetask qgraph \
+pipetask --long-log qgraph \
     -d "skymap='discrete/ci_hsc' AND tract=0 AND patch=69" \
     -b "$2"/butler.yaml \
     --input "$COLLECTION" --output "$FARO_COLLECTION" \
     -p "$FARO_DIR"/pipelines/metrics_pipeline.yaml \
     --save-qgraph "$FARO_QGRAPH_FILE"
 
-pipetask run -j "$1" -b "$2"/butler.yaml \
+pipetask --long-log run -j "$1" -b "$2"/butler.yaml \
     --input "$COLLECTION" --output "$FARO_COLLECTION" \
     --register-dataset-types \
     --qgraph "$FARO_QGRAPH_FILE"
