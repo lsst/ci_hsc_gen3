18c76f95beaaf1a2cabc4a193810eb8ed5cc8ffc
Eli Rykoff <erykoff@stanford.edu>
2022-04-07 11:53:01 -0700

    Add DATA_IDS to lsst.ci.hsc.gen3.

---
+++ python/lsst/ci/hsc/gen3/data.py
@@ -0,0 +1,55 @@
+# This file is part of ci_hsc_gen3.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+DATA_IDS = [
+    {'visit': 903334, 'detector': 16, 'physical_filter': 'HSC-R'},
+    {'visit': 903334, 'detector': 22, 'physical_filter': 'HSC-R'},
+    {'visit': 903334, 'detector': 23, 'physical_filter': 'HSC-R'},
+    {'visit': 903334, 'detector': 100, 'physical_filter': 'HSC-R'},
+    {'visit': 903336, 'detector': 17, 'physical_filter': 'HSC-R'},
+    {'visit': 903336, 'detector': 24, 'physical_filter': 'HSC-R'},
+    {'visit': 903338, 'detector': 18, 'physical_filter': 'HSC-R'},
+    {'visit': 903338, 'detector': 25, 'physical_filter': 'HSC-R'},
+    {'visit': 903342, 'detector': 4, 'physical_filter': 'HSC-R'},
+    {'visit': 903342, 'detector': 10, 'physical_filter': 'HSC-R'},
+    {'visit': 903342, 'detector': 100, 'physical_filter': 'HSC-R'},
+    {'visit': 903344, 'detector': 0, 'physical_filter': 'HSC-R'},
+    {'visit': 903344, 'detector': 5, 'physical_filter': 'HSC-R'},
+    {'visit': 903344, 'detector': 11, 'physical_filter': 'HSC-R'},
+    {'visit': 903346, 'detector': 1, 'physical_filter': 'HSC-R'},
+    {'visit': 903346, 'detector': 6, 'physical_filter': 'HSC-R'},
+    {'visit': 903346, 'detector': 12, 'physical_filter': 'HSC-R'},
+    {'visit': 903986, 'detector': 16, 'physical_filter': 'HSC-I'},
+    {'visit': 903986, 'detector': 22, 'physical_filter': 'HSC-I'},
+    {'visit': 903986, 'detector': 23, 'physical_filter': 'HSC-I'},
+    {'visit': 903986, 'detector': 100, 'physical_filter': 'HSC-I'},
+    {'visit': 904014, 'detector': 1, 'physical_filter': 'HSC-I'},
+    {'visit': 904014, 'detector': 6, 'physical_filter': 'HSC-I'},
+    {'visit': 904014, 'detector': 12, 'physical_filter': 'HSC-I'},
+    {'visit': 903990, 'detector': 18, 'physical_filter': 'HSC-I'},
+    {'visit': 903990, 'detector': 25, 'physical_filter': 'HSC-I'},
+    {'visit': 904010, 'detector': 4, 'physical_filter': 'HSC-I'},
+    {'visit': 904010, 'detector': 10, 'physical_filter': 'HSC-I'},
+    {'visit': 904010, 'detector': 100, 'physical_filter': 'HSC-I'},
+    {'visit': 903988, 'detector': 16, 'physical_filter': 'HSC-I'},
+    {'visit': 903988, 'detector': 17, 'physical_filter': 'HSC-I'},
+    {'visit': 903988, 'detector': 23, 'physical_filter': 'HSC-I'},
+    {'visit': 903988, 'detector': 24, 'physical_filter': 'HSC-I'},
+]
