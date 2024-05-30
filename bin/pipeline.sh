1b50d45f739df288594926a6b7fde24e478fc46e
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2024-05-30 12:32:29 -0400

    Update CI pipeline to use MakeDirectWarp
    instead of MakeWarp.

--- bin/pipeline.sh
+++ bin/pipeline.sh
@@ -62,7 +62,7 @@ pipetask --long-log --log-level="$loglevel" qgraph \
     -c calibrate:astrometry.maxMeanDistanceArcsec=0.025 \
     -c calibrate:requireAstrometry=False \
     -c calibrate:requirePhotoCal=False \
-    -c makeWarp:select.maxPsfTraceRadiusDelta=0.2 \
+    -c makeDirectWarp:select.maxPsfTraceRadiusDelta=0.2 \
     --save-qgraph "$QGRAPH_FILE"
 
 pipetask --long-log --log-level="$loglevel" run \
